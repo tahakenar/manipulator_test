7a352c92897c8f7dfd2e0e4676c7e405d1ac0ed7
tahakenar <53315090+tahakenar@users.noreply.github.com>
2021-05-29 15:56:23 +0300

    joy orient. feature

--- moveit_joystick_control.cpp
+++ moveit_joystick_control.cpp
@@ -5,6 +5,9 @@
 #include <geometry_msgs/Pose.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf/transform_datatypes.h>
+
 static const std::string PLANNING_GROUP = "manipulator";
 float deltaXYZ[3] = {0.0,0.0,0.0};
 float deltaRPY[3] = {0.0,0.0,0.0};
@@ -18,8 +21,9 @@ bool checkJoyDeadband(float data){
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-    float frac = 0.02;
+    float frac = 0.04;
 
+    // cartesian point position control
     float x = joy->axes[1];
     if (checkJoyDeadband(x))
         deltaXYZ[0] = frac*x;
@@ -32,11 +36,30 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     else
         deltaXYZ[1] = 0;
 
-    float z = joy->axes[4];
+    float z = joy->axes[7];
     if (checkJoyDeadband(z))
         deltaXYZ[2] = frac*z;
     else
         deltaXYZ[2] = 0;
+
+    // euler angles orientation control
+    float roll = joy->axes[6];
+    if (checkJoyDeadband(roll))
+        deltaRPY[0] = frac*roll*4;
+    else
+        deltaRPY[0] = 0;
+
+    float pitch = joy->axes[4];
+    if (checkJoyDeadband(pitch))
+        deltaRPY[1] = frac*pitch*4;
+    else
+        deltaRPY[1] = 0;
+
+    float yaw = joy->axes[3];
+    if (checkJoyDeadband(yaw))
+        deltaRPY[2] = frac*yaw*4;
+    else
+        deltaRPY[2] = 0;
 }
 
 
@@ -47,7 +70,7 @@ int main(int argc, char **argv)
     ros::Rate loop_rate(150);
     ros::Subscriber joy_sub = nh.subscribe("joy",1,joyCallback);
 
-    ros::AsyncSpinner spinner(2); //do we need these
+    ros::AsyncSpinner spinner(2); 
     spinner.start();
 
     moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
@@ -56,7 +79,9 @@ int main(int argc, char **argv)
     while (ros::ok())
     {
     ros::spinOnce();
-    ROS_INFO("%f %f %f",deltaXYZ[0],deltaXYZ[1],deltaXYZ[2]);
+    ROS_INFO("%f %f %f ----\n %f %f %f",deltaXYZ[0],deltaXYZ[1],deltaXYZ[2],deltaRPY[0],deltaRPY[1],deltaRPY[2]);
+    //std::vector<double> joint_values = move_group.getCurrentJointValues();
+    //std::cout << joint_values[0] << std::endl;
 
     std::vector<geometry_msgs::Pose> waypoints;
     geometry_msgs::Pose current_pose;
@@ -67,7 +92,7 @@ int main(int argc, char **argv)
         {
             geometry_msgs::Pose target_pose = current_pose;
 
-            target_pose.position.z += deltaXYZ[3];
+            target_pose.position.z += deltaXYZ[2];
             waypoints.push_back(target_pose);
 
             moveit_msgs::RobotTrajectory trajectory;
@@ -116,7 +141,58 @@ int main(int argc, char **argv)
                 move_group.execute(plan);
             }
         }
-    
+
+    // I took the euler-quaternion transformation from ITU rover team 2019 repository
+    // TODO: learn and implement euler-quaternion transform. 
+
+    else if (deltaRPY[0] != 0 && deltaRPY[1] == 0 && deltaRPY[2] == 0) // roll 
+        {
+            geometry_msgs::Pose target_pose = current_pose;
+            tf::Quaternion q_orig, q_rot, q_new;
+            double roll = 2*0.17453*deltaRPY[0], yaw = 0, pitch = 0;
+            q_rot = tf::createQuaternionFromRPY(roll, pitch, yaw);
+            quaternionMsgToTF(target_pose.orientation , q_orig);
+            q_new = q_rot*q_orig;
+            q_new.normalize();
+            quaternionTFToMsg(q_new, target_pose.orientation);
+            move_group.setPoseTarget(target_pose);
+            moveit::planning_interface::MoveGroupInterface::Plan plan;
+            move_group.move();
+
+            }
+
+    else if (deltaRPY[1] != 0 && deltaRPY[0] == 0 && deltaRPY[2] == 0) // pitch 
+        {
+            geometry_msgs::Pose target_pose = current_pose;
+            tf::Quaternion q_orig, q_rot, q_new;
+            double pitch = 2*0.17453*deltaRPY[1], yaw = 0, roll = 0;
+            q_rot = tf::createQuaternionFromRPY(roll, pitch, yaw);
+            quaternionMsgToTF(target_pose.orientation , q_orig);
+            q_new = q_rot*q_orig;
+            q_new.normalize();
+            quaternionTFToMsg(q_new, target_pose.orientation);
+            move_group.setPoseTarget(target_pose);
+            moveit::planning_interface::MoveGroupInterface::Plan plan;
+            move_group.move();
+
+            }
+
+    else if (deltaRPY[2] != 0 && deltaRPY[0] == 0 && deltaRPY[1] == 0) // yaw 
+        {
+            geometry_msgs::Pose target_pose = current_pose;
+            tf::Quaternion q_orig, q_rot, q_new;
+            double yaw = 2*0.17453*deltaRPY[2], pitch = 0, roll = 0;
+            q_rot = tf::createQuaternionFromRPY(roll, pitch, yaw);
+            quaternionMsgToTF(target_pose.orientation , q_orig);
+            q_new = q_rot*q_orig;
+            q_new.normalize();
+            quaternionTFToMsg(q_new, target_pose.orientation);
+            move_group.setPoseTarget(target_pose);
+            moveit::planning_interface::MoveGroupInterface::Plan plan;
+            move_group.move();
+
+            }
+
     loop_rate.sleep();
     
     }  
